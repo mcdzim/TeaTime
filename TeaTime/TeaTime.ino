7a2112cce34b5b31565c7360db7d6f76c253d806
Mike McDonald <michael@castaspes.com>
2020-11-16 20:53:05 +0000

    Updated config

--- TeaTime/TeaTime.ino
+++ TeaTime/TeaTime.ino
@@ -8,16 +8,8 @@ TeaTime v1.0
 #include <Servo.h>
 #include <Adafruit_NeoPixel.h>
 #include <math.h>
+#include "keys.h"
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = "QiKdrooBRMF6Ku8o6uDEoFT1fuqYO_Nf";
-
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "Wifi";
-char pass[] = "Mcwififace1";
 int servo_pin = 2;
 int led_pin = 4;
 int led_pixels = 1;
---
+++ TeaTime/keys.h
@@ -0,0 +1,9 @@
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "BLYNK_KEY";
+
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "SSID";
+char pass[] = "PSK";
