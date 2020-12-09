b8ee515c63db833d998c32b778beba1e0a73d2ce
Mike McDonald <michael@castaspes.com>
2020-12-09 18:57:36 +0000

    Updated with eteaket logo

--- TeaTime/keys.h
+++ TeaTime/keys.h
@@ -1,9 +1,9 @@
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "BLYNK_KEY";
+char auth[] = "QiKdrooBRMF6Ku8o6uDEoFT1fuqYO_Nf";
 
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "SSID";
-char pass[] = "PSK";
+char ssid[] = "Wifi";
+char pass[] = "Mcwififace1";
