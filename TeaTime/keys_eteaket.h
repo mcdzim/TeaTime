ea0883325a22f68a5a4f1531ec74eb0d772394b6
Mike iMac <micmcdzim@gmail.com>
2020-12-09 19:06:49 +0000

    Add eteaket keys

---
+++ TeaTime/keys_eteaket.h
@@ -0,0 +1,9 @@
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "tMTCptwXnGvfVeJ68bxY4LSZF8xM8XSz";
+
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "eteaket";
+char pass[] = "looseleaf";
